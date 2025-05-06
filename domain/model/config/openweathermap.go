294760a7e8b09218c4033afacf74c929ad287af4
Yoshimura Yuu <yyu@mental.poker>
2025-05-06 22:08:11 +0900

    Add `required` to OpenWeatherMapConfiguration

--- domain/model/config/openweathermap.go
+++ domain/model/config/openweathermap.go
@@ -2,7 +2,7 @@ package config
 
 type OpenWeatherMapConfiguration struct {
 	OpenWeatherMapEndPointURL string `env:"OPEN_WEATHER_MAP_ENDPOINT_URL, default=https://api.openweathermap.org"`
-	AppID                     string `env:"OPEN_WEATHER_MAP_APP_ID"`
-	Lat                       string `env:"OPEN_WEATHER_MAP_LAT"`
-	Lon                       string `env:"OPEN_WEATHER_MAP_LON"`
+	AppID                     string `env:"OPEN_WEATHER_MAP_APP_ID, required"`
+	Lat                       string `env:"OPEN_WEATHER_MAP_LAT, required"`
+	Lon                       string `env:"OPEN_WEATHER_MAP_LON, required"`
 }
